093c4f3f9b1a38776e27a5c164eae50166d3fe83
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2023-03-11 19:40:09 -0600

    módulo de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { PagesModule } from './pages/pages.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { PagesModule } from './pages/pages.module';
     AppRoutingModule,
     NgbModule,
     PagesModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
